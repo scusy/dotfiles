ebc9e9bf59918a0cced35754758e73ad686b1f0b
Johannes Lahann <j.lahann@web.de>
2014-10-06 16:54:03 +0200

    change Readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ convinient-dots
 ===============
 
 Default configuration for unix work environment
+
+    curl https://raw.githubusercontent.com/scusy/convinient-dots/master/install.sh | sh
--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ cd ~/
 mkdir .vim
 # get oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
-ln -s  cdots/vimrc .vimrc
-ln -s  cdots/gvimrc .gvimrc
-ln -s  cdots/tmux.conf .tmux.confgv
+git clone https://github.com/scusy/convinient-dots .cdots
+ln -s  .cdots/vimrc .vimrc
+ln -s  .cdots/gvimrc .gvimrc
+ln -s  .cdots/tmux.conf .tmux.confg
 rm .zshrc
 ln -s  cdots/zshrc .zshrc
 #get noebundle
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | s://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
 
